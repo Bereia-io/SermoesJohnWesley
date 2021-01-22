c7beff829d4eb85927d6c7e3ac54e3931f41f039
Jamil Filho <jamilfilho@outlook.com.br>
2021-01-22 18:03:09 -0300

    fix: fix sitemap generator

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   publicRuntimeConfig: {
     SITE_URL: process.env.SITE_URL,
-  }
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require('./lib/sitemapGenerator')
+    }
+
+    return config
+  },
 };
