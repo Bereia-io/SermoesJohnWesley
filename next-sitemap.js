f4c1c2a005f7913b21b5ac11d0bc23997d5ae454
Jamil Filho <jamilfilho@outlook.com.br>
2021-01-22 17:53:20 -0300

    feat: adding sitemap support
    fix #4

---
+++ next-sitemap.js
@@ -0,0 +1,10 @@
+module.exports = {
+    siteUrl: 'https://sermoesjohnwesley.com.br/',
+    generateRobotsTxt: true,
+    // optional
+    robotsTxtOptions: {
+      additionalSitemaps: [
+        'https://sermoesjohnwesley.com.br/sitemap.xml',
+      ],
+    },
+  }
