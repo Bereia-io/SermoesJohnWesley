efe0688417bc7bc45cac635878a9cc63b88abed5
Jamil Filho <jamilfilho@outlook.com.br>
2021-01-23 12:56:41 -0300

    fix: fix google analytics

--- components/analytics.js
+++ components/analytics.js
@@ -1,13 +1,8 @@
-import getConfig from "next/config";
-const { publicRuntimeConfig } = getConfig();
-const env = publicRuntimeConfig.ENV
-const GOOGLE_ANALYTICS_ID = publicRuntimeConfig.GOOGLE_ANALYTICS_ID
-
 export default function Analytics() {
     if(env !== 'dev') {
         return(
             <>
-            <script async src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`}></script>
+            <script async src={`https://www.googletagmanager.com/gtag/js?id=G-HCLENHTT0N`}></script>
             <script src="/assets/scripts/googleanalytics.js"/>
             </>
         )
--- public/assets/scripts/googleanalytics.js
+++ public/assets/scripts/googleanalytics.js
@@ -6,4 +6,4 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
-gtag('config', `${GOOGLE_ANALYTICS_ID}`);
+gtag('config', 'G-HCLENHTT0N');
