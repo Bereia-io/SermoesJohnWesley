fd8271ca89b1fca955521ada45260fce4ded8a81
Jamil Filho <jamilfilho@outlook.com.br>
2021-01-23 11:35:51 -0300

    fix: google analytics id

--- public/assets/scripts/googleanalytics.js
+++ public/assets/scripts/googleanalytics.js
@@ -1,5 +1,9 @@
+import getConfig from "next/config";
+const { publicRuntimeConfig } = getConfig();
+const GOOGLE_ANALYTICS_ID = publicRuntimeConfig.GOOGLE_ANALYTICS_ID
+
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
-gtag('config', 'G-HCLENHTT0N');
+gtag('config', `${GOOGLE_ANALYTICS_ID}`);
