0265ec467e29035f342f91016d8831a35b4efdb9
Jamil Filho <jamilfilho@outlook.com.br>
2021-01-23 17:05:52 -0300

    revert: remove google analytics
    removing google analytics until a definitive solution is applied

--- components/analytics.js
+++ components/analytics.js
@@ -1,8 +1,6 @@
 export default function Analytics() {
     return(
         <>
-        <script async src={`https://www.googletagmanager.com/gtag/js?id=G-HCLENHTT0N`}></script>
-        <script src="/assets/scripts/googleanalytics.js"/>
         </>
     ) 
 }
--- public/assets/scripts/googleanalytics.js
+++
@@ -1,9 +0,0 @@
-import getConfig from "next/config";
-const { publicRuntimeConfig } = getConfig();
-const GOOGLE_ANALYTICS_ID = publicRuntimeConfig.GOOGLE_ANALYTICS_ID
-
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'G-HCLENHTT0N');
